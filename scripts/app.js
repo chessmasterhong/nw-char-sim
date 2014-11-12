a917392beded33d78afe9b2eccb28f251eedfe97
Kevin Chan <chessmasterhong@yahoo.com>
2014-11-12 17:43:17 -0500

    Added placeholder routes and views.

--- scripts/app.js
+++ scripts/app.js
@@ -8,5 +8,25 @@ angular.module('nwcs', [
         .state('home', {
             url: '',
             templateUrl: 'partials/home.html'
+        })
+        .state('character', {
+            url: '/character',
+            templateUrl: 'partials/character.html'
+        })
+        .state('powers', {
+            url: '/powers',
+            templateUrl: 'partials/powers.html'
+        })
+        .state('feats', {
+            url: '/feats',
+            templateUrl: 'partials/feats.html'
+        })
+        .state('boons', {
+            url: '/boons',
+            templateUrl: 'partials/boons.html'
+        })
+        .state('companions', {
+            url: '/companions',
+            templateUrl: 'partials/companions.html'
         });
 });
