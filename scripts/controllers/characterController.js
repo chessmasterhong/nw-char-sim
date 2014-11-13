ccaec67db9c12f76b49816e5c77bf9c8874f2ba0
Kevin Chan <chessmasterhong@yahoo.com>
2014-11-13 11:10:29 -0500

    Added placeholder for character stat ratings.

--- scripts/controllers/characterController.js
+++ scripts/controllers/characterController.js
@@ -4,6 +4,25 @@ define([
     'use strict';
 
     controllers.controller('characterController', function($scope) {
-
+        $scope.charStatRatings = [
+            { name: 'Power',                  value: 0 },
+            { name: 'Critical Strike',        value: 0 },
+            { name: 'Armor Penetration',      value: 0 },
+            { name: 'Recovery',               value: 0 },
+            { name: 'Action Point Gain',      value: 0 },
+            { name: 'Combat Advantage Bonus', value: 0 },
+            { name: 'Control Bonus',          value: 0 },
+            { name: 'Defense',                value: 0 },
+            { name: 'Deflect',                value: 0 },
+            { name: 'Regeneration',           value: 0 },
+            { name: 'Tenacity (PVP)',         value: 0 },
+            { name: 'Stamina Gain',           value: 0 },
+            { name: 'Incoming Healing Bonus', value: 0 },
+            { name: 'AoE Resist',             value: 0 },
+            { name: 'Control Resist',         value: 0 },
+            { name: 'Movement',               value: 0 },
+            { name: 'Companion Influence',    value: 0 },
+            { name: 'Gold Gain',              value: 0 }
+        ];
     });
 });
